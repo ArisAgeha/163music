ae65dcba3a58b63d8c66ad1b785da08547e4f43e
Ageha <87725092@qq.com>
2018-06-27 19:00:17 +0800

    active upload function

---
+++ src/structure.js
@@ -0,0 +1,20 @@
+CollectionList ['JPOP', '纯音乐', '翻唱'];
+
+JPOP {
+    [
+        {
+            name: '',
+            artist: '',
+            album: '',
+            link: '',
+            size: '',
+        },
+        {
+            name: '',
+            artist: '',
+            album: '',
+            link: '',
+            size: '',
+        }
+	],
+}
