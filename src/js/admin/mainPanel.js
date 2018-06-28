b6e1c29443d8bf00ded51454d3d84bddc33e63d0
Ageha <87725092@qq.com>
2018-06-28 21:00:45 +0800

    require songlist complete & watchAddCollectionList-demo

--- src/js/admin/mainPanel.js
+++ src/js/admin/mainPanel.js
@@ -16,26 +16,69 @@ let eventHub = require('./eventHub.js');
         </tr>`
     };
     
-    let model = {};
+    let model = {
+        init() {
+            this.songList = {}
+        },
+
+        requireSongList() {
+            let queryList = new AV.Query('SongList');
+            queryList.find().then((list) => {
+                for (let i = 0; i < list.length; i++) {
+                    let obj = {...list[i].attributes};
+                    let id = list[i].id;
+                    this.songList[id] = obj;
+                }
+            }, (err) => {
+                console.log(err)    
+            })
+            
+        },
+    };
 
     let controller = {
         init(view, model) {
             this.view = view;
             this.model = model;
+            this.getSongList();
             this.bindEvent();
+        },
+
+        getSongList() {
+            this.model.requireSongList();
         },
         
         bindEvent() {
-            this.watchSwitchCollectionList();
+            this.watchAddList();
+            this.watchSwitchList();
         },
 
-        watchSwitchCollectionList() {
+        watchAddList() {
+            // {songList: [id1, id2, id3...]}
+            eventHub.on('addCollection', (data) => {
+                let tbody = $('tbody');
+                for(let songID of data) {
+                    let songInfo = this.model.songList.songID;
+                    let trString = this.view.template
+                        .replace('__name__', songInfo.name);
+                        .replace('__artist__', songInfo.artist);
+                        .replace('__album__', songInfo.album);
+                        .replace('__link__', songInfo.link);
+                        .replace('__size', songInfo.size);
+                        .replace('__saveStatus__', '已保存')
+                    let tr = $(trString);
+                    tbody.append(tr);
+                }
+            })
+        },
+
+        watchSwitchList() {
             eventHub.on('switchCollection', (data) => {
                 let $el = $(this.view.el);
                 $el.find('._' + `${data}`).addClass('show').siblings().removeClass('show');
             })
         }
     };
-
+    model.init();
     controller.init(view, model);
 }
