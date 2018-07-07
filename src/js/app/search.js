48b4f9b9bee21c9e902a3f6ab40b32cd8239ecfc
Ageha <87725092@qq.com>
2018-07-07 09:23:23 +0800

    searchSong - JS && fix bug: imfor -> infor

--- src/js/app/search.js
+++ src/js/app/search.js
@@ -15,11 +15,11 @@ let view = {
     </li>`,
 
     render(data) {
-        let searchWrapper = $(this.el).find('.searchWrapper');
-        let itemString = template.replace('__songName__', data.songName)
+        let searchWrapper = $(this.el).find('.search-wrapper');
+        let itemString = this.template.replace('__songName__', data.songName)
                                     .replace('__artist__', data.artist)
                                     .replace('__album__', data.album);
-        let item = $(resultString);
+        let item = $(itemString);
         item.prop('id', data.id);
         searchWrapper.append(item);
     }
@@ -50,11 +50,16 @@ let model = {
 //        });
 //    }
     async queryData(string) {
-        console.log(string)
-        let query = new AV.Query('SongList');
-        query.matches(name, '^About')
+        let regExp = new RegExp(string, "i");
+        let query1 = new AV.Query('SongList');
+        let query2 = new AV.Query('SongList');
+        let query3 = new AV.Query('SongList');
+        query1.matches("name", regExp);
+        query2.matches("artist", regExp);
+        query3.matches("album", regExp);
+        let query = new AV.Query.or(query1, query2, query3)
+
         return await query.find().then(function(results) {
-            console.log(results)
             return results;
         });
     }
@@ -75,10 +80,30 @@ let controller = {
         let $el = $(this.view.el);
         let button = $el.find('.searchButton');
         button.on('click touch', async () => {
+            await search.call(this);
+        });
+        $el.find('input').bind('keypress', async (e) => {
+            if (e.keyCode === 13){
+                await search.call(this);
+            }
+        });
+
+        async function search() {
             let value = $el.find('input').val();
             let result = await this.model.queryData(value);
+            if (!value) return;
             console.log(result);
-        })
+            for (let item of result) {
+                let data = {
+                    id: item.id,
+                    songName: item.attributes.name,
+                    artist: item.attributes.artist,
+                    album: item.attributes.album
+                }
+                this.view.render(data);
+            }
+
+        }
     }
 
 }
--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-let a = {}
-console.log(!!a)
+console.log(!!'')
