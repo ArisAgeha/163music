30c048f12e816591c6ae60d5afe060a010cfa08a
Ageha <87725092@qq.com>
2018-07-07 08:07:20 +0800

    search query testing

--- src/js/app/search.js
+++ src/js/app/search.js
@@ -26,7 +26,61 @@ let view = {
 }
 
 let model = {
-    init() {
-        this.result = [];
+//    async queryData(string) {
+//        console.log(string)
+//        var query = new AV.SearchQuery('SongList');
+//        query.queryString(`name:${string} artist:${string} album:${string}`);
+//        return await query.find().then(function(results) {
+//            console.log(results)
+//            return results;
+//        });
+//    }
+//    async queryData(string) {
+//        console.log(string)
+//        var query1 = new AV.Query('SongList');
+//        var query2 = new AV.Query('SongList');
+//        var query3 = new AV.Query('SongList');
+//        query1.contains('name', string);
+//        query2.contains('artist', string);
+//        query3.contains('album', string);
+//        let query = new AV.Query.or(query1, query2, query3);
+//        return await query.find().then(function(results) {
+//            console.log(results)
+//            return results;
+//        });
+//    }
+    async queryData(string) {
+        console.log(string)
+        let query = new AV.Query('SongList');
+        query.matches(name, '^About')
+        return await query.find().then(function(results) {
+            console.log(results)
+            return results;
+        });
     }
 }
+
+let controller = {
+    init() {
+        this.view = view;
+        this.model = model;
+        this.bindEvent();
+    },
+
+    bindEvent() {
+        this.watchSearch();
+    },
+
+    async watchSearch() {
+        let $el = $(this.view.el);
+        let button = $el.find('.searchButton');
+        button.on('click touch', async () => {
+            let value = $el.find('input').val();
+            let result = await this.model.queryData(value);
+            console.log(result);
+        })
+    }
+
+}
+
+controller.init();
